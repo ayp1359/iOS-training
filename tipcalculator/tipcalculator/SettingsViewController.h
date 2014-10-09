9d51f0be0d9d01cfbb4502eceeb1f4a25142f287
ayp1359 <ayp1359@yahoo.com>
2014-10-08 21:39:41 -0700

    more progress

---
+++ tipcalculator/tipcalculator/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  tipcalculator
+//
+//  Created by Ali YAZDAN PANAH on 10/8/14.
+//  Copyright (c) 2014 Ali YAZDAN PANAH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
